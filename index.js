d1fd29bac743669e0c82e98335c0f4fbd19fd0a0
Oleg Kasenbekov <oleg.kasenbekov@enterra-inc.com>
2024-09-14 17:01:12 +0700

    forum

--- index.js
+++ index.js
@@ -11,58 +11,52 @@ const bot = new Telegraf(process.env.API_KEY_BOT)
 const localSession = new LocalSession({
     database: 'sessions.json',
     state: {
-        messages: []
+        topics: []
     }
 })
 
 bot.use(localSession.middleware())
 
+
+// create topic when user start bot
 bot.start(async ctx => {
     if (isAdmin(ctx.update.message.chat.id)) await ctx.reply('Hi, admin')
-    else await ctx.reply('Hi, mate. Please send me some messages and i will resend them to support')
-})
-
-bot.on(message('reply_to_message', 'text'), async (ctx) => {
-    if (isAdmin(ctx.update.message.chat.id)) {
-        const chatId = ctx.sessionDB
-            .get('messages')
+    else {
+        const userTopic = ctx.sessionDB
+            .get('topics')
             .value()
-            .find((message) => message.messageId === ctx.update.message.reply_to_message.message_id)
-            .chatId
+            .find((tp) => tp.chatId === ctx.update.message.chat.id)
 
-        await ctx.sendMessage(ctx.update.message.text, {chat_id: chatId})
+        if (!!userTopic) return;
+
+        const newTopic = await ctx.createForumTopic(ctx.update.message.from.username, {chat_id: process.env.ADMIN_CHAT_ID})
+        ctx.sessionDB.get('topics').push({
+            ...newTopic,
+            chatId: ctx.update.message.chat.id
+        }).write()
     }
 })
 
 bot.on(message('text'), async (ctx) => {
-    await ctx.reply(`Well, I\`ll send this to my admin)`)
+    if (isAdmin(ctx.update.message.chat.id)) {
+        const topic = ctx.sessionDB
+            .get('topics')
+            .value()
+            .find((tp) => tp.message_thread_id === ctx.update.message.message_thread_id)
 
-    const isFirstMessage = !ctx.sessionDB
-        .get('messages')
-        .value()
-        .find((message) => message.chatId === ctx.update.message.chat.id)
+        if (!topic) return;
 
-    let newMessageId
-
-    if (isFirstMessage) {
-        const newMessage = await ctx.forwardMessage(
-            process.env.ADMIN_CHAT_ID,
-        )
-        newMessageId = newMessage.message_id
+        await ctx.copyMessage(topic.chatId)
     } else {
-        const newMessage = await ctx.reply(
-            ctx.update.message.text,
-            {
-                chat_id: process.env.ADMIN_CHAT_ID,
-                reply_parameters: {
-                    message_id: ctx.session.lastMessageId
-                }
-            })
-        newMessageId = newMessage.message_id
-    }
+        const userTopic = ctx.sessionDB
+            .get('topics')
+            .value()
+            .find((tp) => tp.chatId === ctx.update.message.chat.id)
 
-    ctx.sessionDB.get('messages').push({chatId: ctx.update.message.chat.id, messageId: newMessageId}).write()
-    ctx.session.lastMessageId = newMessageId
+        if (!userTopic) return;
+
+        await ctx.copyMessage(process.env.ADMIN_CHAT_ID, {message_thread_id: userTopic.message_thread_id})
+    }
 })
 
 bot.launch()
